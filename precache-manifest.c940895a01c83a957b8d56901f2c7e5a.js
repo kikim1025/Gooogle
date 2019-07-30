b525668e31b28143be126543fcf588fe5f3cb6f2
Ki <kikim1025@gmail.com>
2019-07-30 08:48:10 -0400

    Updates

---
+++ precache-manifest.c940895a01c83a957b8d56901f2c7e5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3ad17b2f1526f10fe3066f30bc1490",
+    "url": "/Gooogle/index.html"
+  },
+  {
+    "revision": "23b87660c35a3e10a477",
+    "url": "/Gooogle/static/css/main.afc6b398.chunk.css"
+  },
+  {
+    "revision": "dff8f5e6896d4e47fb65",
+    "url": "/Gooogle/static/js/2.d16ba94f.chunk.js"
+  },
+  {
+    "revision": "23b87660c35a3e10a477",
+    "url": "/Gooogle/static/js/main.b8ebbd5f.chunk.js"
+  },
+  {
+    "revision": "86e5a416afca49c17bd0",
+    "url": "/Gooogle/static/js/runtime~main.fccab590.js"
+  }
+]);
