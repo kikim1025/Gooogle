24092d3434bf2c94879377a8e42634134e7d3e5e
Ki <kikim1025@gmail.com>
2019-07-30 08:26:30 -0400

    added a local state in search to keep track of input

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import Button from './Button';
 
-const Search = () => (
-    <section className='search'>
-        <input className='search__input' />
-        <div className='search__buttons'>
-            <Button name='Google Search' onClick={''} style='button--gray left' />
-            <Button name="I'm Feeling Lucky" onClick={''} style='button--gray right' />
-        </div>
-    </section>
-);
+class Search extends React.Component {
+    
+    state = {
+        input: ''
+    };
+
+    getInput = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    render() {
+        return (
+            <section className='search'>
+            <input className='search__input' name='input' onChange={this.getInput} />
+            <div className='search__buttons'>
+                <Button name='Google Search' onClick={''} style='button--gray left' />
+                <Button name="I'm Feeling Lucky" onClick={''} style='button--gray right' />
+            </div>
+            </section>
+        );
+    };
+};
 
 export default Search;
